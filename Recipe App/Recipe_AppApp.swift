1454024f958d2d5d040df199883bf0d5a4a419ed
VaibhavRajani <vaibhav.rajanivit@gmail.com>
2024-05-30 14:14:37 -0400

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Vaibhav Rajani on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
